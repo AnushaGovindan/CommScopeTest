e978afb7cc6ca5924664def112e6ad8badab34b8
anusha <anusha.govindan1@gmail.com>
2022-03-13 22:28:28 -0400

    removed <result1>

--- src/main/java/com/test/CommScopeTest.java
+++ src/main/java/com/test/CommScopeTest.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class CommScopeTest<result1> {
+public class CommScopeTest {
 
     WebDriver webDriver;
     private String url = "https://www.commscope.com";
